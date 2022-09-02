2602401d907c66c2f7abcf5be8bcd7c9a4ddc9a6
Alex V <alexvap23@gmail.com>
2022-09-02 23:41:36 +0300

    update lesson 228

--- src/loginRoutes.ts
+++ src/loginRoutes.ts
@@ -1,9 +1,18 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 
 interface RequestWithBody extends Request {
     body: { [key: string]:string | undefined}
 }
 
+function requireAuth(req: Request, res: Response, next: NextFunction) {
+    if(req.session && req.session.loggedIn) {
+        next();
+        return;
+    }
+
+    res.status(403);
+    res.send('Not permitted')
+}
 const router = Router();
 
 router.get('/login', (req:Request, res:Response) => {
@@ -33,5 +42,33 @@ router.post('/login', (req: RequestWithBody, res: Response) => {
         res.send('Invalid email or password')
     }
 });
+
+router.get('/', (req, res) => {
+    if(req.session && req.session.loggedIn) {
+        res.send(`
+        <div>
+            <div>You are logged in</div>
+            <a href="/logout">Logout</a>
+        </div>
+        `)
+    } else {
+        res.send(`
+        <div>
+            <div>You are not logged in</div>
+            <a href="/login">Login</a>
+        </div>
+        `)
+    }
+});
+
+router.get('/logout', (req, res) => {
+    req.session = undefined;
+    res.redirect('/');
+});
+
+router.get('/protected', requireAuth, (req: Request, res: Response) => {
+    res.send('You got page!')
+});
+
 
 export { router };
