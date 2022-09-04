0ca57bf84b8d9b4429609673bc65a953839f4726
Alex V <alexvap23@gmail.com>
2022-09-04 22:32:38 +0300

    Lesson 248

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
-import { router } from './loginRoutes';
+import { router } from './routes/loginRoutes';
 import express from 'express';
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
+import { AppRouter } from './AppRouter';
+
+import './controllers/LoginController';
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieSession({keys: ['daslkdaslfj']}));
 app.use(router);
+app.use(AppRouter.getInstance());
 
 app.listen(3000, () => {
     console.log('Listening on port 3000')
